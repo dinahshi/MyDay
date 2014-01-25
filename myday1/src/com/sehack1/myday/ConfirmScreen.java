2eee859f7a738e4c71cb6d9205486b03b82fafc8
Dinah Shi <dinahshi28@gmail.com>
2014-01-25 03:01:06 -0500

    Bitmap resizing added

--- myday1/src/com/sehack1/myday/ConfirmScreen.java
+++ myday1/src/com/sehack1/myday/ConfirmScreen.java
@@ -1,11 +1,15 @@
 package com.sehack1.myday;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.io.PrintStream;
 
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
@@ -19,17 +23,74 @@ import android.widget.ImageView;
 
 public class ConfirmScreen extends Activity {
 	
-	private Uri mImgUri;
+    private static final String TAG = "ConfirmScreen";
+    private static final int SIDE_LENGTH = 350;
+
+	private ImageView imageView;
 	
 	public String caption = "No caption chosen";
 	public String date = "No date yet";
 	public String imageInfo = "Default";
+	public String filePath;
 	
 	EditText captionEditText;
 	ImageButton post;
 	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.confirm);
+		Log.d(TAG, "confirm created");
+		
+		// Compress and paint image
+		Intent i = getIntent();
+		filePath = i.getStringExtra(NewDayScreen.MESSAGE);		 
+        Log.d(TAG, "Filepath retrieved: " + filePath);
+		imageView.setImageBitmap(decodeSampledBitmapFromResource(filePath, SIDE_LENGTH, SIDE_LENGTH));
+        
+		addListenerOnButton();
+	}
+	
+	public static Bitmap decodeSampledBitmapFromResource(String filePath, int reqWidth, int reqHeight) {
+
+	    // First decode with inJustDecodeBounds=true to check dimensions
+	    final BitmapFactory.Options options = new BitmapFactory.Options();
+	    options.inJustDecodeBounds = true;
+		BitmapFactory.decodeFile(filePath, options);
+
+	    // Calculate inSampleSize
+	    options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+	    // Decode bitmap with inSampleSize set
+	    options.inJustDecodeBounds = false;
+	    return BitmapFactory.decodeFile(filePath, options);
+	}
+	
+	public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+		// Raw height and width of image
+		final int height = options.outHeight;
+		final int width = options.outWidth;
+		int inSampleSize = 1;
+
+		if (height > reqHeight || width > reqWidth) {
+
+			final int halfHeight = height / 2;
+			final int halfWidth = width / 2;
+
+			// Calculate the largest inSampleSize value that is a power of 2 and keeps both
+			// height and width larger than the requested height and width.
+			while ((halfHeight / inSampleSize) > reqHeight && (halfWidth / inSampleSize) > reqWidth) {
+				inSampleSize *= 2;
+			}
+		}
+	
+		Log.d(TAG, "sample size: " + inSampleSize);
+		return inSampleSize;
+	}
+	
 	public String createMessage() {
-		return mImgUri + "!#!#!#!#" + caption + "!#!#!#!#" + date;
+		return filePath + "!#!#!#!#" + caption + "!#!#!#!#" + date;
 	}
 	public void saveMessage(View view) {
 		String filename = "textfile.txt";
@@ -49,24 +110,7 @@ public class ConfirmScreen extends Activity {
 			e.printStackTrace();
 		}
 	}
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.confirm);
-		
-		// Paint the image 
-		Intent i = getIntent();
-		mImgUri = Uri.parse(i.getStringExtra(NewDayScreen.MESSAGE));		 
-        ImageView imageView = (ImageView) findViewById(R.id.ivUserPic);
-        try {
-        	imageView.setImageBitmap(MediaStore.Images.Media.getBitmap(this.getContentResolver(), mImgUri));
-        }
-        catch (Exception E){}
-        
-		addListenerOnButton();
-	}
-
+	
 	public void addListenerOnButton() {
 		 
 		post = (ImageButton) findViewById(R.id.bPost);
--- myday1/src/com/sehack1/myday/NewDayScreen.java
+++ myday1/src/com/sehack1/myday/NewDayScreen.java
@@ -5,7 +5,9 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -16,15 +18,15 @@ import android.widget.ImageButton;
 
 public class NewDayScreen extends Activity implements View.OnClickListener{
 
-	private ImageButton takePic;
-	private ImageButton choosePic;
-	private Uri mImgUri = null;
-	
-    public static final int MEDIA_TYPE_IMAGE = 1;
+	private static final String TAG = "NewDayScreen";
 	public static final int RESULT_LOAD_IMG = 1;
 	public static final int RESULT_TAKE_PIC = 2;
 	public final static String MESSAGE = "com.sehack1.myday.PICTUREPATH";
 
+	private ImageButton takePic;
+	private ImageButton choosePic;
+	private Uri mImgUri = null;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -47,11 +49,12 @@ public class NewDayScreen extends Activity implements View.OnClickListener{
 	public void onClick(View v) {
 		switch (v.getId()) {
 			case R.id.bTakePic:
-				Log.d ("tag", "starting camera intent");
+				Log.d (TAG, "starting camera intent");
 				Intent capture = new Intent (MediaStore.ACTION_IMAGE_CAPTURE);
-				mImgUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+				mImgUri = getOutputMediaFileUri();
 				capture.putExtra(MediaStore.EXTRA_OUTPUT, mImgUri);
 				startActivityForResult(capture, RESULT_TAKE_PIC);
+				Log.d(TAG, "activity started");
 				break;
 			case R.id.bChoosePic:
 				Intent i = new Intent (Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
@@ -60,12 +63,12 @@ public class NewDayScreen extends Activity implements View.OnClickListener{
 		}
 	}
 	
-	private static Uri getOutputMediaFileUri(int type){
-        return Uri.fromFile(getOutputMediaFile(type));
+	private static Uri getOutputMediaFileUri(){
+        return Uri.fromFile(getOutputMediaFile());
 	}
 	
 	/** Create a File for saving an image or video */
-    private static File getOutputMediaFile(int type){
+    private static File getOutputMediaFile(){
             // To be safe, you should check that the SDCard is mounted
             // using Environment.getExternalStorageState() before doing this.
 
@@ -81,14 +84,7 @@ public class NewDayScreen extends Activity implements View.OnClickListener{
 
             // Create a media file name
             String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-            File mediaFile;
-            if (type == MEDIA_TYPE_IMAGE){
-                    mediaFile = new File(mediaStorageDir.getPath() + File.separator +
-                                    "IMG_"+ timeStamp + ".jpg");
-            } else {
-                    return null;
-            }
-
+            File mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_"+ timeStamp + ".jpg");
             return mediaFile;
     }
 	
@@ -97,34 +93,41 @@ public class NewDayScreen extends Activity implements View.OnClickListener{
 		super.onActivityResult(requestCode, resultCode, data);
 		
 		if(requestCode == RESULT_TAKE_PIC && resultCode == RESULT_OK) {
-	        Log.d("tag", "picture saved in: " + mImgUri.toString());
+	        Log.d(TAG, "picture saved in: " + mImgUri.toString());
 	        
 	        // Open up captions page
             Intent openConfirm = new Intent ("com.sehack1.myday.CONFIRMSCREEN");
-			openConfirm.putExtra(MESSAGE, mImgUri);
+			openConfirm.putExtra(MESSAGE, getPathFromURI(mImgUri));
+			Log.d(TAG, "start confirm screen");
             startActivity(openConfirm);	
 		}
 			
 		if (requestCode == RESULT_LOAD_IMG && resultCode == RESULT_OK && data != null)
 		{
 			mImgUri = data.getData();
-	        Log.d("tag", "picture retrieved from: " + mImgUri.toString());
+	        Log.d(TAG, "picture retrieved from: " + mImgUri.toString());
 
             // Open up captions page
             Intent openConfirm = new Intent ("com.sehack1.myday.CONFIRMSCREEN");
-			openConfirm.putExtra(MESSAGE, mImgUri.toString());
-			Log.d("tag", "startactivity next");
+			openConfirm.putExtra(MESSAGE, getPathFromURI(mImgUri));
+			Log.d(TAG, "start confirm screen");
             startActivity(openConfirm);		            
 		}
 	}
 	
-	/*private String getPathFromURI(Uri contentUri) {
-		String[] proj = {MediaStore.Images.Media.DATA};
-	    Cursor cursor = getContentResolver().query(contentUri, proj, null, null, null);
-	    cursor.moveToFirst();
-	    int columnIndex = cursor.getColumnIndex(proj[0]);
-        String picturePath = cursor.getString(columnIndex);
-        cursor.close();
-        return picturePath;
-    }*/
+	public String getPathFromURI(Uri contentUri) {
+		Cursor cursor = null;
+		try { 
+		String[] proj = { MediaStore.Images.Media.DATA };
+		cursor = getContentResolver().query(contentUri,  proj, null, null, null);
+		int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+		cursor.moveToFirst();
+		return cursor.getString(column_index);
+		} 
+		finally {
+			if (cursor != null) {
+				cursor.close();
+		    }
+		}
+	}
 }
